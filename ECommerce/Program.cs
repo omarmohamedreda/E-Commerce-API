87d20a4b5b5950161c6060b4878b7d783e463824
omarmohamedreda <mrm227420@gmail.com>
2025-10-29 02:11:20 +0300

    OnionArchitecture&DataSeeding

--- ECommerce/Program.cs
+++ ECommerce/Program.cs
@@ -1,4 +1,9 @@
 
+using ECommerce.Domain.Contracts.Seed;
+using ECommerce.Presistence.Contexts;
+using ECommerce.Presistence.DataSeed;
+using Microsoft.EntityFrameworkCore;
+
 namespace ECommerce
 {
     public class Program
@@ -13,7 +18,28 @@ namespace ECommerce
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            #region Add Configuration DB Service
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            #region DataSeeding
+            builder.Services.AddScoped<IDataSeeding, DataSeeding>();
+            #endregion
+
+            #endregion
+
+
+
+
+
             var app = builder.Build();
+
+            #region Services
+            var scope = app.Services.CreateScope();
+            var ObjectSeeding = scope.ServiceProvider.GetRequiredService<IDataSeeding>();
+            ObjectSeeding.DataSeed(); 
+            #endregion
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
