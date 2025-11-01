af17c684ef2973336115b1357cf760c25b094ee3
omarmohamedreda <mrm227420@gmail.com>
2025-11-01 23:17:08 +0200

    ServiceManager

---
+++ Core/ECommerce.Services/ServiceManager.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ECommerce.Abstraction;
+using ECommerce.Domain.Contracts.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Services
+{
+    internal class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper) : IServiceManager
+    {
+
+        private readonly Lazy<IProductServices> _LazyProductServices = new Lazy<IProductServices>(() => new ProductServices(_unitOfWork, _mapper));
+
+        public IProductServices ProductServices => _LazyProductServices.Value;
+    }
+}
